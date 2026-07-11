e8b4383f8bccffbcaad5532825f5926e7216a9da
calllum19 <cmcfadyen567@gmail.com>
2026-07-11 21:31:42 +0700

    approach complete; but it was wrong

--- appledivision.cpp
+++ appledivision.cpp
@@ -3,15 +3,31 @@
 using namespace std;
 using ll = long long;
 
-ll minweight(const vector<ll> &P) {}
+ll appledivision(const vector<ll> &p) {
+  vector<ll> values;
+  ll heldvalue1 = 0;
+  ll heldvalue2 = 0;
+  for (int i = 1; i <= p.size(); ++i) {
+    for (int k = 0; k <= i - 1; ++k) {
+      heldvalue1 += p[k];
+    }
+    for (int j = p.size() - 1; j >= i; --j) {
+      heldvalue2 += p[j];
+    }
+    values.push_back(abs(heldvalue1 - heldvalue2));
+    heldvalue1 = 0;
+    heldvalue2 = 0;
+  }
+  return *min_element(values.begin(), values.end());
+}
 
 int main() {
   int N;
   cin >> N;
-  vector<ll> P(N);
+  vector<ll> p(N);
   for (int i = 0; i < N; ++i) {
-    cin >> P[i];
+    cin >> p[i];
   }
 
-  cout << minweight(P);
+  cout << appledivision(p);
 }
