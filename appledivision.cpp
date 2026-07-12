41a3ba8c1d567d106018d0cc6319f265cefca22d
calllum19 <cmcfadyen567@gmail.com>
2026-07-12 08:57:54 +0700

    fixed approach; last one was contiguous, but this i learned to gen subsets, prolly need to solidify this with variants

--- appledivision.cpp
+++ appledivision.cpp
@@ -1,33 +1,28 @@
+#include <algorithm>
 #include <bits/stdc++.h>
 #include <vector>
 using namespace std;
 using ll = long long;
 
-ll appledivision(const vector<ll> &p) {
-  vector<ll> values;
-  ll heldvalue1 = 0;
-  ll heldvalue2 = 0;
-  for (int i = 1; i <= p.size(); ++i) {
-    for (int k = 0; k <= i - 1; ++k) {
-      heldvalue1 += p[k];
-    }
-    for (int j = p.size() - 1; j >= i; --j) {
-      heldvalue2 += p[j];
-    }
-    values.push_back(abs(heldvalue1 - heldvalue2));
-    heldvalue1 = 0;
-    heldvalue2 = 0;
+ll appledivision(ll index, ll csum, ll totalweight, const vector<ll> &p) {
+  if (index == p.size()) {
+    return abs(2 * csum - totalweight);
+  } else {
+    ll exc = appledivision(index + 1, csum, totalweight, p);
+    ll inc = appledivision(index + 1, csum + p[index], totalweight, p);
+    return min(exc, inc);
   }
-  return *min_element(values.begin(), values.end());
 }
 
 int main() {
   int N;
   cin >> N;
   vector<ll> p(N);
+  ll totalweight = 0;
   for (int i = 0; i < N; ++i) {
     cin >> p[i];
+    totalweight += p[i];
   }
 
-  cout << appledivision(p);
+  cout << appledivision(0, 0, totalweight, p);
 }
