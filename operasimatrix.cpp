a727a2416211b6cef9cba2aa2de05950cfad7282
calllum19 <cmcfadyen567@gmail.com>
2026-07-18 20:09:13 +0700

    needs fixing

--- operasimatrix.cpp
+++ operasimatrix.cpp
@@ -3,7 +3,18 @@
 #include <vector>
 using namespace std;
 
-int operations(vector<vector<int>> M1, string &method, int &derajat, int &X) {
+vector<vector<int>> transpose(const vector<vector<int>> &M1) {
+  vector<vector<int>> M2(M1[0].size(), vector<int>(M1.size(), 0));
+  for (int i = 0; i < M2.size(); ++i) {
+    for (int j = 0; j < M2[0].size(); ++j) {
+      M2[i][j] = M1[j][i];
+    }
+  }
+  return M2;
+}
+
+vector<vector<int>> operations(vector<vector<int>> M1, string &method,
+                               int &derajat, int &X) {
   for (int i = 0; i < X; ++i) {
     if (method == "_") {
       reverse(M1.begin(), M1.end());
@@ -14,13 +25,24 @@ int operations(vector<vector<int>> M1, string &method, int &derajat, int &X) {
     }
     switch (derajat) {
     case 90:
+      M1 = transpose(M1);
+      for (int j = 0; j < M1.size(); ++j) {
+        reverse(M1.at(j).begin(), M1.at(j).end());
+      }
       break;
     case 180:
+      reverse(M1.begin(), M1.end());
+      for (int j = 0; j < M1.size(); ++j) {
+        reverse(M1.at(j).begin(), M1.at(j).end());
+      }
       break;
     case 270:
+      M1 = transpose(M1);
+      reverse(M1.begin(), M1.end());
       break;
     }
   }
+  return M1;
 }
 
 int main(int argc, char *argv[]) {
@@ -37,7 +59,7 @@ int main(int argc, char *argv[]) {
   int derajat;
   cin >> method >> derajat;
 
-  operations(M1, method, derajat, inputX);
+  M1 = operations(M1, method, derajat, inputX);
   // results printed
   int count = 0;
   for (const auto &row : M1) {
