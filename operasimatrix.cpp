1486e7596c75c5574ca3451f9e696d418a5ed7c9
calllum19 <cmcfadyen567@gmail.com>
2026-07-18 17:54:01 +0700

    need to find out how to transpose

--- operasimatrix.cpp
+++ operasimatrix.cpp
@@ -1,6 +1,27 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 using namespace std;
+
+int operations(vector<vector<int>> M1, string &method, int &derajat, int &X) {
+  for (int i = 0; i < X; ++i) {
+    if (method == "_") {
+      reverse(M1.begin(), M1.end());
+    } else if (method == "|") {
+      for (int j = 0; j < M1.size(); ++j) {
+        reverse(M1.at(j).begin(), M1.at(j).end());
+      }
+    }
+    switch (derajat) {
+    case 90:
+      break;
+    case 180:
+      break;
+    case 270:
+      break;
+    }
+  }
+}
 
 int main(int argc, char *argv[]) {
   int inputN, inputM, inputX;
@@ -13,13 +34,22 @@ int main(int argc, char *argv[]) {
     }
   }
   string method;
-  cin >> method;
-  if (method == "_") {
-    reflect(M1, 0);
-  } else if (method == "|") {
-    reflect(M1, 1);
-  } else if (method == "90") {
-    reflect
+  int derajat;
+  cin >> method >> derajat;
+
+  operations(M1, method, derajat, inputX);
+  // results printed
+  int count = 0;
+  for (const auto &row : M1) {
+    for (int value : row) {
+      count++;
+      if (count < inputN) {
+        cout << value << " ";
+      } else {
+        cout << value << "\n";
+        count = 0;
+      }
+    }
   }
   return 0;
 }
