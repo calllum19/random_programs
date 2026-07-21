eb9e5fe33154db49a814d2a26d140aeaeaabe3f7
calllum19 <cmcfadyen567@gmail.com>
2026-07-21 13:57:48 +0700

    input done, but how to algo???

--- basketball.cpp
+++ basketball.cpp
@@ -1,2 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int kemenangan(vector<int> P) {}
+
+int main(int argc, char *argv[]) {
+  int N, D;
+  cin >> N >> D;
+
+  vector<int> P(N);
+  for (int i = 0; i < N; ++i) {
+    cin >> P[i];
+  }
+
+  cout << kemenangan(P);
+  return 0;
+}
